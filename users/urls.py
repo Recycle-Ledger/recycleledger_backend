4a3958f3b1618bfa428d88eef92a92ccd3129453
이태검 <taegeom0601@gmail.com>
2022-05-09 10:35:04 +0900

    ModelSerializer로 변경 및 유저 모델 수정 부분 추가

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create',user_signup,name="signup"),
     path('login',MyTokenObtainPairView.as_view(),name="login"),
     path('logout',TokenBlacklistView.as_view(),name="logout"),
+    path('info_update',user_info_update,name="info_update"),
     # as_view는 클래스 진입 메소드 
 ]
--- users/views.py
+++ users/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status #응답코드용 
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import *
+from django.contrib.auth import get_user_model
+import json
 # Create your views here.
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def user_signup(request): #회원가입
-    serializer=UserSerializer(data=request.data)
-    if serializer.is_valid(): #UserSerializer에 validate
-        token = serializer.save()
+    userserializer=UserSerializer(data=request.data)
+    if userserializer.is_valid(raise_exception=True): #UserSerializer validate
+        token = userserializer.save()
         return Response(token,status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -21,11 +23,35 @@ def user_signup(request): #회원가입
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class=MyTokenObtainPairSerializer
 
+
+@api_view(['PUT'])    
+# @permission_classes([IsAuthenticated]) #회원으로 인증된 요청 한해서 view 호출
+def user_info_update(request): #회원정보 수정
+    body=json.loads(request.body)
+    User=get_user_model()
+    me=get_object_or_404(User,phone_num=body["phone_num"])
+
+    # print(me)
+    
+    userserializer=UserSerializer(me,data=request.data)
+    # print(userserializer.is_valid())
+    if userserializer.is_valid():
+        token=userserializer.save()
+        # print(token)
+        return Response(status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# request에 {"phone_num":"","update_data":{}}
+
 '''
     회원가입시 POST
     {
-        "wallet_addr":"전자지갑주소",
-        "business_num":"사업자등록번호",
+        "phone_num":"01024280249",
+        "username":"이태검",
+        # "wallet_addr":"",
+        # "business_num":"",
         "password":"패스워드"
     }
     return 
@@ -37,16 +63,15 @@ class MyTokenObtainPairView(TokenObtainPairView):
 '''
 '''
     로그인시 POST 
-    {   
-        "wallet_addr":"전자지갑주소",
+    {
+        "phone_num":"01024280249",
         "password":"패스워드"
     }
     return
     {
         "refresh": "토큰",
         "access": "토큰",
-        "wallet_addr": "지갑주소",
-        "business_num": "사업자등록번호"
+
     }
 '''
 '''
