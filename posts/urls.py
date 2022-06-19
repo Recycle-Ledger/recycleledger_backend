44f1e5901e97e103e94bff42ba539e12482fec46
lhs961021 <lhs961021@naver.com>
2022-06-19 18:15:52 +0900

    check

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = 'posts'
 
 urlpatterns = [
-    path('main/',main,name="main"),
-    path('main_j/',main_j,name="main_j"),
-    path('main_c/',main_c,name="main_c"),
-    path('login/',login,name="login")
+    path('main',main,name="main"),
+    path('main_j',main_j,name="main_j"),
+    path('main_c',main_c,name="main_c"),
+    path('login',login,name="login")
 ]
